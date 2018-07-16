2cc46649a18323a0aa604daf07867943cd62af3b
Eli Steiger <eli.steiger@hyland.com>
2018-07-16 17:34:10 -0400

    bunch o linq

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
 namespace Introduction
 {
@@ -11,28 +11,42 @@ namespace Introduction
   {
     static void Main(string[] args)
     {
-      string path = @"C:\windows";
-      ShowLargeFilesWithoutLinq(path);
-    }
+      //      string path = @"C:\windows";
+      //      ShowLargeFilesWithoutLinq(path);
+      //      Console.WriteLine("*******");
+      //      ShowLargeFilesWithLinq(path);
+      //    }
 
-    private static void ShowLargeFilesWithoutLinq(string path)
-    {
-      DirectoryInfo directory = new DirectoryInfo(path);
-      FileInfo[] files = directory.GetFiles();
-      Array.Sort(files, new FileInfoComparer());  
-      for (int i = 0; i < 5; i++)
-      {
-        FileInfo file = files[i];
-        Console.WriteLine($"{file.Name, -20}:{file.Length, 10:N0}");
-      }
-    }
-  }
+      //    //private static void ShowLargeFilesWithLinq(string path)
+      //    //{
+      //    //  var query = from file in new Directory(path).GetFileSystemEntries()
+      //    //              .OrderByDescending(f => f.Length)
+      //    //              .Take(5);
 
-  public class FileInfoComparer : IComparer<FileInfo>
-  {
-    public int Compare(FileInfo x, FileInfo y)
-    {
-      return y.Length.CompareTo(x.Length);
+      //      foreach (var file in query.Take(5))
+      //      {
+      //        Console.WriteLine($"{file.Name,-20}:{file.Length,10:N0}");
+      //      }
+      //    }
+
+      //    private static void ShowLargeFilesWithoutLinq(string path)
+      //    {
+      //      DirectoryInfo directory = new DirectoryInfo(path);
+      //      FileInfo[] files = directory.GetFiles();
+      //      Array.Sort(files, new FileInfoComparer());  
+      //      for (int i = 0; i < 5; i++)
+      //      {
+      //        FileInfo file = files[i];
+      //        Console.WriteLine($"{file.Name, -20}:{file.Length, 10:N0}");
+      //      }
+      //    }
+      //  }
+
+      //  public class FileInfoComparer : IComparer<FileInfo>
+      //  {
+      //    public int Compare(FileInfo x, FileInfo y)
+      //    {
+      //      return y.Length.CompareTo(x.Length);
     }
   }
 }
---
+++ Features/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//using Features.Linq;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Features
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      Func<int, int> square = x => x * x;
+      Func<int, int, int> add = (x, y) => {
+        int temp = x + y;
+        return temp;
+        };
+
+      Action<int> write = x => Console.WriteLine(x);
+
+      write(square(add(3, 5)));
+
+      IEnumerable<Employee> developers = new Employee[]
+        {
+          new Employee { Id = 1, Name = "SFoo5" },
+          new Employee { Id = 2, Name = "Bar45" }
+        };
+
+      IEnumerable<Employee> sales = new List<Employee>()
+      {
+        new Employee { Id = 3, Name = "Ricky Bobby"}
+      };
+
+      //foreach (var person in developers)
+      //{ 
+      //  Console.WriteLine(person.Name);
+      //}
+
+      //Console.WriteLine(developers.OrderBy(x => x.Id));
+
+      //IEnumerator<Employee> enumerator = developers.GetEnumerator();
+      //while (enumerator.MoveNext())
+      //{
+      //  Console.WriteLine(enumerator.Current.Name);
+      //}
+      var query = developers.Where(e => e.Name.Length == 5)
+        .OrderByDescending(e => e.Name)
+        .Select(e => e);
+
+      var query2 = from developer in developers
+                   where developer.Name.Length == 5
+                   orderby developer.Name descending
+                   select developer;
+
+      foreach (Employee employee in query2)
+      {
+        Console.WriteLine(employee.Name);
+      }
+    }
+  }
+}
